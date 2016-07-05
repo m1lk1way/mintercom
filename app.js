51c101d219b6030f6ee25549edde79ee57e90b9b
m1lk1way <alexeyandras@gmail.com>
2016-07-05 22:01:06 +0300

    reworked

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var http = require('http').Server(express);
 var bodyParser = require('body-parser');
 var app = express();
 var io = require('socket.io')(http);*/
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
